a074325712a0b610f0412da222828fb80e32ceb2
Youshlen Nadesan <103027561+Youshlen11@users.noreply.github.com>
2022-04-20 15:11:34 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,38 @@
 import requests
 
-request_api = requests.post("https://sfrs-j75dy.ondigitalocean.app/api/listDistricts")
-#print(request_api.status_code)
-#print(request_api.json())
+get_request_api = requests.post("https://sfrs-j75dy.ondigitalocean.app/api/listDistricts")
+#print(get_request_api.status_code)
+#print(get_request_api.json())
 
-i=0
+count_array = 0
+number_of_Districts = 1
 
-print ("{:<40} {:<30}".format('Municipalities','Province'))
+print('|-----------------------------------------|-------------------------------|')
+print ("{:<10} {:<30} {:<10} {:<20} {:<10}".format('|','Municipalities','|','Province','|'))
+print('|-----------------------------------------|-------------------------------|')
 
-for i in range(0,11):
-    municipalities=request_api.json()['data'][int(i)]['name']
-    province=request_api.json()['data'][int(i)]['province']['name']
-    municipalities+=" District"
-    print ("{:<40} {:<30}".format(municipalities,province))
-    i+=1
-    municipalities=""
+for count_array in range(0,11):
+    get_district_municipalities = get_request_api.json()['data'][int(count_array)]['name']
+    get_province = get_request_api.json()['data'][int(count_array)]['province']['name']
+    get_district_municipalities = str(number_of_Districts) + ") "+ get_district_municipalities + " District"
+    number_of_Districts += 1
 
+    print ("{:<10} {:<30} {:<10} {:<20} {:<10}".format('|',get_district_municipalities,'|',get_province,'|'))
+    
+    count_array += 1                                                                                                                                                                                                             
+    get_district_municipalities = ""
+
+print('|-----------------------------------------|-------------------------------|')
+print(r"""\
+
+                               ._ o o
+                               \_`-)|_
+                            ,""       \ 
+                          ,"  ## |   ಠ ಠ. 
+                        ," ##   ,-\__    `.
+                      ,"       /     `--._;)
+                    ,"     ## /
+                  ,"   ##    /
+
+
+            """)
