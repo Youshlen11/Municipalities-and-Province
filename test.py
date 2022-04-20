9fd837e224207f3b3c5fab00cacc73969f1d25f8
Youshlen Nadesan <103027561+Youshlen11@users.noreply.github.com>
2022-04-20 11:53:46 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import requests
+
+request_api = requests.post("https://sfrs-j75dy.ondigitalocean.app/api/listDistricts")
+#print(request_api.status_code)
+#print(request_api.json())
+
+i=0
+
+print ("{:<40} {:<30}".format('Municipalities','Province'))
+
+for i in range(0,11):
+    municipalities=request_api.json()['data'][int(i)]['name']
+    province=request_api.json()['data'][int(i)]['province']['name']
+    municipalities+=" District"
+    print ("{:<40} {:<30}".format(municipalities,province))
+    i+=1
+    municipalities=""
+
