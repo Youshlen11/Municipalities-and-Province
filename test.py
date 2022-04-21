558575ee09dac25a40681ef2d87ebb7fb4b57136
Youshlen Nadesan <103027561+Youshlen11@users.noreply.github.com>
2022-04-21 12:39:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,14 +4,19 @@ get_request_api = requests.post("https://sfrs-j75dy.ondigitalocean.app/api/listD
 #print(get_request_api.status_code)
 #print(get_request_api.json())
 
+count_elements = len(get_request_api.json()['data'])
 count_array = 0
 number_of_Districts = 1
 
+print(count_elements)
+
+
+
 print('|-----------------------------------------|-------------------------------|')
-print ("{:<10} {:<30} {:<10} {:<20} {:<10}".format('|','Municipalities','|','Province','|'))
+print("{:<10} {:<30} {:<10} {:<20} {:<10}".format('|','Municipality','|','Province','|'))
 print('|-----------------------------------------|-------------------------------|')
 
-for count_array in range(0,11):
+for count_array in range(0,count_elements):
     get_district_municipalities = get_request_api.json()['data'][int(count_array)]['name']
     get_province = get_request_api.json()['data'][int(count_array)]['province']['name']
     get_district_municipalities = str(number_of_Districts) + ") "+ get_district_municipalities + " District"
